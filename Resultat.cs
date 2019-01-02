7f18df12197fff716bbd646fcd1bb96108906406
vappy2 <43212809+vappy2@users.noreply.github.com>
2019-01-02 19:39:06 +0100

    upload souce

---
+++ Resultat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_n_1
+{
+    class Resultat
+    {
+        private int tempsRealise;
+        Epreuve uneEpreuve;
+        Participant unParticipant;
+
+        public Resultat(Participant unParticipant, Epreuve uneEpreuve, int tempsRealise)
+        {
+            this.tempsRealise = tempsRealise;
+            this.uneEpreuve = uneEpreuve;
+            this.unParticipant = unParticipant;
+        }
+        public Epreuve GetEpreuve()
+        {
+            return this.uneEpreuve;
+        }
+        public int GetTempsRealise()
+        {
+            return this.tempsRealise;
+        }
+    }
+}
